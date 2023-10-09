275afa8d2a756803ed6bc956eab693494156e08a
LSX666666666666 <147392170+LSX666666666666@users.noreply.github.com>
2023-10-09 19:05:38 +0800

    Add files via upload

---
+++ Login.aspx
@@ -0,0 +1,68 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="Hotel.WebForm1" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>计科1班 32021010048 李圣贤 酒店预订管理系统</title>
+    <link rel="stylesheet" href="Assets/Libraries/Bootstrap/css/bootstrap.min.css" />
+    <style>
+        /*背景图片*/
+        body{
+            background-image:url(../Assets/Images/hotel22.jpg);
+            /*background-image:url(../Assets/Images/hotel4.jpg);*/
+            /*background-image:url(../Assets/Images/hotel22.webp);*/
+            background-size:cover;
+        }
+        /*样式透明度*/
+        .container-fluid{
+            opacity:0.9;
+        }
+    </style>
+</head>
+<body> 
+       <!-- 网页中央文字 -->
+    <form id="form1" runat="server">
+        <div>
+            <div class="container-fluid">
+        <div class="row" style="height:200px"></div>
+        <div class="row">
+            <div class="col-md-4"></div>
+            <div class="col-md-4 bg-light rounded-3">
+                <h1 class="text-text-success text-center">欢迎登录酒店预订管理系统</h1>
+                <form>
+  <div class="mb-3">
+       <!-- 账号密码 -->
+    <label for="UserTb" class="form-label">账号名</label>
+    <input type="text" class="form-control" id="UserTb" runat="server" >
+  </div>
+  <div class="mb-3">
+    <label for="PasswordTb" class="form-label">密码</label>
+    <input type="password" class="form-control" id="PasswordTb" runat="server" >
+  </div>
+  <!-- label选择管理员或用户 -->
+  <div class="mb-3">
+      <label id="ErrMsg" class="text-danger" runat="server"></label>
+    <input type="radio" id="AdminCb" runat="server" name="Role"><label class="text-success">管理员</label>
+    <input type="radio" id="UserCb" runat="server" name="Role"><label class="text-success">用户</label>
+
+  </div>
+                    <div class="d-grid">       
+                        <asp:Button ID="LoginBtn" runat="server" Text="登录" class="btn btn-success btn-block" OnClick="LoginBtn_Click" />
+                    </div>
+                    <br />
+                    <div class="d-grid">       
+                        <asp:Button ID="Button1" runat="server" Text="注册" class="btn btn-success btn-block" OnClick="signup_Click" />
+                    </div>
+                    <br />
+</form>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+        
+    </div>
+        </div>
+    </form>
+</body>
+</html>
---
+++ Login.aspx.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Hotel
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Con = new Models.Functions();
+            Session["UserName"] = "";
+            Session["UId"] = "";
+        }
+
+        Models.Functions Con;
+        static string DecryptFileContent(string filePath)
+        {
+            string decryptedText = string.Empty;
+
+            // 检查文件是否存在
+            if (File.Exists(filePath))
+            {
+                try
+                {
+                    // 读取文件内容
+                    using (StreamReader reader = new StreamReader(filePath))
+                    {
+                        string encryptedText = reader.ReadToEnd();
+
+                        // 解密每个字符并拼接到解密文本中
+                        foreach (char c in encryptedText)
+                        {
+                            char decryptedChar = (char)(c - 4);
+                            decryptedText += decryptedChar;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("An error occurred while decrypting the file: " + ex.Message);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found: " + filePath);
+            }
+
+            return decryptedText;
+        }
+
+
+        protected void LoginBtn_Click(object sender, EventArgs e)
+        {
+           
+            string basePath = Directory.GetCurrentDirectory(); // 获取当前工作目录
+            string relativePath1 = "\\username.txt"; // 相对路径
+            string usernamepath = Path.Combine(basePath, relativePath1); // 组合路径
+            string username = File.ReadAllText(usernamepath);
+
+            
+            string relativePath2 = "\\password.txt"; // 相对路径
+            string passwordpath = Path.Combine(basePath, relativePath2); // 组合路径
+           // string password = File.ReadAllText(passwordpath);
+
+
+            string password = DecryptFileContent(passwordpath);
+
+
+
+
+            //Response.Redirect("https://localhost:44312/View/Admin/Rooms.aspx");
+            if (AdminCb.Checked)
+            {
+
+                if (UserTb.Value == username && PasswordTb.Value == password)
+                {
+
+                    Session["UserName"] = username;
+                    Response.Redirect("https://localhost:44312/View/Admin/Rooms.aspx");
+
+                }
+                else
+                {
+                    ErrMsg.InnerText = "无效的管理员！！！";
+                }
+            }
+            else
+            {
+                string Query="select UId,UName,UPass from UserTbl where UName='{0}' and UPass='{1}'";
+                Query = string.Format(Query, UserTb.Value, PasswordTb.Value);
+                DataTable dt = Con.GetData(Query);
+                if (dt.Rows.Count == 0)
+                {
+                    ErrMsg.InnerText = "无效的用户！！！";
+                }
+                else
+                {
+                    Session["UserName"] = dt.Rows[0][1].ToString();
+                    Session["UId"] = dt.Rows[0][0].ToString();
+                    Response.Redirect("https://localhost:44312/View/Users/Booking.aspx");
+                }
+            }
+        }
+
+        protected void signup_Click(object sender, EventArgs e)
+        {
+
+            //Response.Redirect("https://localhost:44312//View//Users//signup2.aspx");
+            Response.Redirect("https://localhost:44312/View/Users/signup2.aspx");
+        }
+    }
+}
